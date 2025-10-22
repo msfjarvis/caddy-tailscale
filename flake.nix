c403d11ba3bec0a918518d306f169cc6e26432ce
Harsh Shandilya <me@msfjarvis.dev>
2025-10-22 15:05:09 +0530

    fix(nix): init devShell

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "devshell for a Golang project";
+
+  inputs.nixpkgs.url = "github:msfjarvis/nixpkgs/nixpkgs-unstable";
+
+  inputs.systems.url = "github:msfjarvis/flake-systems";
+
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.devshell.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.flake-compat.url = "git+https://git.lix.systems/lix-project/flake-compat";
+  inputs.flake-compat.flake = false;
+
+  outputs =
+    {
+      systems,
+      nixpkgs,
+      devshell,
+      ...
+    }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = eachSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [ devshell.overlays.default ];
+          };
+        in
+        {
+          default = pkgs.devshell.mkShell {
+            bash = {
+              interactive = "";
+            };
+
+            env = [
+              {
+                name = "DEVSHELL_NO_MOTD";
+                value = 1;
+              }
+            ];
+
+            packages = with pkgs; [
+              gcc
+              git
+              go-outline
+              go
+              gopls
+              gotools
+              xcaddy
+            ];
+          };
+        }
+      );
+    };
+}
